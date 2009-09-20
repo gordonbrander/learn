0827643af4dde70ac1fd36624298bc71f90c1f10
gordonbrander <marlfoskr@gmail.com>
2009-09-19 20:03:13 -0600

    Added further comments to sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,5 @@
 <?php /*
-This file is used by 
-
+This file is usually used to hold a sidebar, or aside area for your theme. It's optional, like most files in your theme. It's pulled into other files using the get_sidebar() function.
 */ ?>
 
 <hr />
@@ -9,7 +8,9 @@ This file is used by
 <?php /*
 	Start a dynamic sidebar.
 	Anything that goes inside of this if statement is used as fallback content. The fallback content can be overridden by adding widgets to this area via the WP admin.
-	You can see where this sidebar is registered in functions.php. */
+	You can see where this sidebar is registered in functions.php.
+	<http://codex.wordpress.org/Function_Reference/dynamic_sidebar>
+*/
 	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-0') ) : ?>
 	<div class="widget">
 		<?php get_search_form(); ?>
